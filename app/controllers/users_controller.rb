5ebfa8184879d9bbb608d68a0a3c742b7e310914
takazawa1204 <ckicken1204@gmail.com>
2021-12-13 02:53:15 +0000

    mypage#8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,28 +6,32 @@ class UsersController < ApplicationController
  end
 
  def create
-   @user = User.new(user_params)
-   if @user.save
-     redirect_to login_path
-   else
-     render 'new'
-   end
+  @user = User.new(user_params)
+  if @user.save
+    redirect_to login_path
+  else
+    render 'new'
+  end
  end
 
  def show
-    @user = @current_user
+  @user = @current_user
  end
 
  def edit
+  @user = @current_user
  end
 
  def update
+  @user = @current_user
+  @user.update(user_params)
+  redirect_to users_my_page_path
  end
 
  private
 
  def user_params
-   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+   params.require(:user).permit(:name, :email, :password, :password_confirmation, :level)
  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password validations: true
+  after_initialize :set_default_values
+  # Use this if you wanna set default values only when creating a new record.
+  #after_initialize :set_default_values, if: :new_record?
 
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
@@ -10,5 +13,11 @@ class User < ApplicationRecord
 
   def self.encrypt(token)
     Digest::SHA256.hexdigest(token.to_s)
+  end
+
+  private
+
+  def set_default_values
+    self.level ||= 1
   end
 end
